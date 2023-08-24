fa929ed141abc7feaa141b3dd196f6e996141bcd
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-08-24 19:25:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let listContainer=document.getElementById("infi-list")
 for(let i=0;i<20;i++){
 	const listiitem=document.createElement("li")
 	
-	listiitem.textContent=`Item-${i}`
+	listiitem.textContent=`Item ${i}`
 	// console.log("called2")
 
 	listContainer.appendChild(listiitem)
