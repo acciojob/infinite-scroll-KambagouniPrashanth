e3df2f620ad458c5ddd0e45692021b13ae2dbc38
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-08-24 19:03:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let listContainer=document.getElementById("infi-list")
 for(let i=0;i<20;i++){
 	const listiitem=document.createElement("li")
 	li.innerHtml=`list${i}`
+	listContainer.append(li)
 
 }
 
